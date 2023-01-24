b7d6bba49fe0992645273815b8e37cbfd97e9c8a
HeeJuChae <smileweek92@gmail.com>
2023-01-24 21:47:39 +0900

    first commit

---
+++ src/main/java/com/oslog/OslogApplication.java
@@ -0,0 +1,13 @@
+package com.oslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OslogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OslogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oslog/OslogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oslog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OslogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
