89ffee608c61aba86d50d74378d0b7b068ad9b42
HeeJuChae <smileweek92@gmail.com>
2023-01-25 22:13:31 +0900

    data validation part1

---
+++ src/main/java/com/oslog/request/PostCreate.java
@@ -0,0 +1,17 @@
+package com.oslog.request;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+
+@Setter
+@Getter
+@ToString
+@Builder
+public class PostCreate {
+
+    @NotBlank(message = "타이틀을 입력해주세요.")
+    private String title;
+    @NotBlank(message = "콘텐츠을 입력해주세요.")
+    private String content;
+}
